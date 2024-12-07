0c8cc5d4010a3e7e11470a87c8b415fbdfa90906
jasonMB12 <112223099+jasonmoyaB@users.noreply.github.com>
2024-12-07 14:22:51 -0600

    recordatorio mediicamentos
    se agrego los recordatorios

--- public/showMedications.php
+++ public/showMedications.php
@@ -4,39 +4,54 @@ include 'header.php';
 
 <main class="main__showApp">
     <div>
-        <h2>Medicamentos agregados</h2>
-    <table class="table">
-        <thead class="bg-info">
-            <tr>
-                <th class="col">Nombre Medicamento</th>
-                <th class="col">Descripcion</th>
-                <th class="col">Dosis</th>
-                <th class="col">Frecuencia</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                include '../config/conexion.php';
-                $sql = $conn->query(' select * from Medicamentos ');
-                while ($datos = $sql->fetch_object()){ ?>
-                        <tr>
-                            <td><?= $datos->nombre ?></td>
-                            <td><?= $datos->descripcion ?></td>
-                            <td><?= $datos->dosis ?></td>
-                            <td><?= $datos->frecuencia ?></td>
-                            <td>
-                                <a href="" class="btn btn-small btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
-  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-  <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
-</svg></a>
-                                <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
-              <?php  }
-            ?>
-            
-        </tbody>
-    </table>
+        <!-- Notificaciones de medicamentos -->
+        <?php
+        include '../config/conexion.php';
+
+        // Consulta para obtener medicamentos próximos
+        $sql_medicamentos = $conn->query("
+            SELECT * FROM Medicamentos 
+            WHERE horario >= CURTIME() 
+            AND fecha = CURDATE() 
+            ORDER BY horario ASC 
+            LIMIT 3
+        ");
+
+        if ($sql_medicamentos->num_rows > 0) { ?>
+            <div class="alert alert-warning">
+                <h4>Próximos Medicamentos:</h4>
+                <ul>
+                    <?php while ($med = $sql_medicamentos->fetch_object()) { ?>
+                        <li><strong><?= $med->horario ?></strong>: Tomar <?= $med->dosis ?> de <?= $med->nombre ?>.</li>
+                    <?php } ?>
+                </ul>
+            </div>
+        <?php } ?>
+
+        <!-- tabla de medicamentos -->
+        <table class="table">
+            <thead class="bg-info">
+                <tr>
+                    <th class="col">Nombre</th>
+                    <th class="col">Descripción</th>
+                    <th class="col">Dosis</th>
+                    <th class="col">Frecuencia</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                //mostrar todos los medicamentos
+                $sql = $conn->query('SELECT * FROM Medicamentos');
+                while ($datos = $sql->fetch_object()) { ?>
+                    <tr>
+                        <td><?= $datos->nombre ?></td>
+                        <td><?= $datos->descripcion ?></td>
+                        <td><?= $datos->dosis ?></td>
+                        <td><?= $datos->frecuencia ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </main>
 
